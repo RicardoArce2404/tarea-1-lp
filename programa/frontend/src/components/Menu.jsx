ca9fbda2ec9b002cfb61904c814f99101e867410
Ricardo Arce <yasserarce2404@gmail.com>
2025-03-09 23:45:57 -0600

    fin

--- programa/frontend/src/components/Menu.jsx
+++ programa/frontend/src/components/Menu.jsx
@@ -2,14 +2,16 @@ export function Menu({ handleStartButton, handleHistoryButton }) {
   return (
     <div className="menu">
       <div className="menu-player-names">
-        <input type="text" id="name1" placeholder="Jugador 1"/>
-        <input type="text" id="name2" placeholder="Jugador 2"/>
+        <input type="text" id="name1" placeholder="Jugador 1" />
+        <input type="text" id="name2" placeholder="Jugador 2" />
       </div>
       <div className="menu-options">
         <button type="button" onClick={handleStartButton}>
           Iniciar Juego
         </button>
-        <button type="button" onClick={handleHistoryButton} >Ver historial de resultados</button>
+        <button type="button" onClick={handleHistoryButton}>
+          Ver historial de resultados
+        </button>
       </div>
     </div>
   );
